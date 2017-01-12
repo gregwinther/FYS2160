7a7441b86871b868a978bca39599670b8157fd05
gregwinther <sebastwi@uio.no>
2017-01-12 18:55:32 +0100

    Added scripts and figure to cheat sheet

--- programs/randomstate.py
+++ programs/randomstate.py
@@ -1,17 +1,15 @@
 import numpy as np
-from numpy.random import permutation
 
-
-def randomstate(NA, NB, M):
+def randomstate(NA, M):
     m = np.zeros((M,M))
-    index = permutation(M*M)
+    index = np.random.permutation(M*M)
     ix, iy = np.unravel_index(index, (M,M))
     j = range(NA)
     m[ix[j], iy[j]] = 1
-    j = range(NA, NA+NA)
+    j = range(NA, NA+(M*M-NA))
     m[ix[j], iy[j]] = -1
     return m
 
-m = randomstate(32, 32, 8)
+m = randomstate(32, 8)
 
 print(m)
